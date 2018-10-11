c4e6692cffeebb0606350b019b520b26b5b14f18
Carolyn Whelpley <carolynwhelpley@Jeffs-MacBook-Pro-3.local>
2018-10-11 15:50:20 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :roster, :school_name
+  
+  def initialize(school_name)
+    @roster = Hash.new 
+    @roster = roster
+  end
+  
+  def add_student(student, grade)
+   @roster[grade] ||= []
+   @roster[grade] << student
+  end
+    
+  def grade(student)
+    @roster[student]
+  end
+
+  def sort()
+    new_roster = Hash.new
+    @roster.each do |grade, student|
+      new_roster[grade] = student.sort
+    end
+    new_roster
+  end
+  
+
+end
