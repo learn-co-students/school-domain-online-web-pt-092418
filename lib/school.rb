097856b95c18d56b6525bb6e4b419e7543be6dcf
Kevin J Gomez <kjgmez1@gmail.com>
2018-10-12 20:29:20 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,8 @@
 class School
+  attr_reader :roster
   def initialize(school_name)
     @school_name= school_name
     @roster={}
-  end
-  def roster
-    @roster
   end
   def add_student(name, grade)
     @name=name
