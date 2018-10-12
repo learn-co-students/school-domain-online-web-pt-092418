e6c48c0be3a8bc9e85645ff114ca67a7ab860e77
Kevin J Gomez <kjgmez1@gmail.com>
2018-10-12 23:25:35 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -5,9 +5,7 @@ class School
     @roster={}
   end
   def add_student(name, grade)
-    @name=name
-    @grade=grade
-    @roster.include?(@grade) ? @roster[@grade] << @name : @roster[@grade]=[@name] #ternary == conditional ? if true : if fasle (else)
+    @roster.include?(grade) ? @roster[grade] << name : @roster[grade]=[name] #ternary == conditional ? if true : if fasle (else)
   end
   def grade(grade)
     @roster[grade]
