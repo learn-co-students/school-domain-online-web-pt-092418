0c91c3f1ba535aabbd23274deb7680d9d82b030a
Kevin J Gomez <kjgmez1@gmail.com>
2018-10-12 20:22:15 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -9,12 +9,12 @@ class School
   def add_student(name, grade)
     @name=name
     @grade=grade
-    @roster.include?(@grade) ? @roster[@grade] << @name : @roster[@grade]=[@name]
+    @roster.include?(@grade) ? @roster[@grade] << @name : @roster[@grade]=[@name] #ternary == conditional ? if true : if fasle (else)
   end
   def grade(grade)
     @roster[grade]
   end
   def sort
-    @roster.each{|key,value| value.sort!}
+    @roster.each{|grade, names| names.sort!} #sort! will change the value we are sorting. sort wont return sorted
   end
 end
