00dea3881dc408d045975b6cfe69ecb51d77c314
Virginia Dooley <vcdooley@gmail.com>
2018-10-12 11:32:44 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,11 @@
-# code here!
+class School
+  def intialize(roster)
+    @roster
+    def roster
+      @name = name
+      @grade = grade
+    end 
+    
+    def add_student(name, grade)
+  end 
+end
