56cf29f240e4a062ab6d4b40c35800ef200335ab
Virginia Dooley <vcdooley@gmail.com>
2018-10-14 15:33:06 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,24 +1,38 @@
 class School
+  attr_reader :roster
+  attr_accessor :roster
+  
   def initialize(name)
     @name = name
     @roster = {}
   end 
   
-  def roster
-    @roster
+  def name
+    @name
   end 
   
+  # def roster
+  #   @roster
+  # end 
+  
+  # def roster=(hash)
+  #   @roster = hash
+  # end 
+  
   def add_student(name, grade)
-    roster[grade] ||= []
-    roster[grade] << name
-    
-    def grade(grade)
-     
+    if roster.keys.include?(grade)
+      @roster[grade] << name
+    else   
+      @roster[grade] = [name]
     end 
+  end
     
-    def sort(roster, name) 
+  def grade(grade)
+    roster[grade] 
+  end 
+    
+  def sort(roster, name) 
     # @roster ||= School.sort_by(roster[:grade])
-      School.sort_by(@roster[:name])
-    end 
+    School.sort_by(@roster[:name])
   end 
 end
