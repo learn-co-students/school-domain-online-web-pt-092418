1963257d97bdc380819dd5ee6ef7b7d3ad8993ff
Patrick Shim <patrickshim@Patricks-MBP.fios-router.home>
2018-10-19 13:03:17 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(student)
+    @roster[student]
+  end
+
+  def sort
+    new_roster= Hash.new
+    @roster.each do |grade, student|
+      new_roster[grade]= student.sort
+  end
+    new_roster
+  end
+end
